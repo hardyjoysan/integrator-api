1d62894ef393594beff342d78b19dec86ebf7a53
hardyjoysan <hardyjoysan@gmail.com>
2018-10-18 20:45:56 +0530

    API Basic configuration done

---
+++ lib/app.ts
@@ -0,0 +1,25 @@
+import * as express from 'express';
+import * as bodyparser from 'body-parser';
+import {Routes} from "./routes/Routes";
+
+class App{
+
+    public app: express.Application;
+    public routePrv: Routes = new Routes();
+
+    constructor() {
+        this.app = express();
+        this.config();
+        this.routePrv.routes(this.app);
+    }
+
+    private config(): void{
+        //support application/json post data
+        this.app.use(bodyparser.json());
+
+        //support application/x-www-form-urlencoded post data
+        this.app.use(bodyparser.urlencoded({ extended : false }));
+    }
+}
+
+export default new App().app;
---
+++ lib/routes/Routes.ts
@@ -0,0 +1,17 @@
+import {Request, Response} from "express";
+
+export class Routes{    
+    
+    public routes(app): void{
+
+        app.route('/')
+        .get((req: Request, res: Response) => {
+            res.status(200).send({
+                message: 'Welcome Home!'
+            })
+        });
+
+
+
+    }
+}
